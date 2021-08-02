79715d6bff21398d57c8428b225e86bf9116499b
Harveyj7 <harveyy7@gmail.com>
2021-08-02 11:34:27 +0100

    prettified

--- api.js
+++ api.js
@@ -1,15 +1,5 @@
 let canvas, context, Val_max, Val_min, sections, xScale, yScale;
 // Values for the Data Plot, they can also be obtained from a external file
-const tbody = document.querySelector("tbody");
-const tRow = document.createElement("tr");
-const tData = document.createElement("td");
-const tData2 = document.createElement("td");
-tbody.appendChild(tRow);
-tRow.appendChild(tData);
-tRow.appendChild(tData2);
-tData.innerHTML='hey'
-tData2.innerHTML='hey'
-
 
 function init(xAxis, yAxis) {
   // set these values for your data
@@ -20,36 +10,50 @@ function init(xAxis, yAxis) {
   let stepSize = 10;
   let columnSize = 50;
   let rowSize = 50;
-  let margin = 10;
+  let margin = 0;
+// console.log(sections)
 
   canvas = document.getElementById("canvas");
   context = canvas.getContext("2d");
-  context.fillStyle = "#0099ff";
+  context.fillStyle = "white";
+  context.fillRect(50, 50, canvas.width, canvas.height);
+  // context.background = "white";
   context.font = "20 pt Verdana";
+  context.fontColor = "black";
 
   yScale = (canvas.height - columnSize - margin) / (Val_max - Val_min);
   xScale = (canvas.width - rowSize) / sections;
 
-  context.strokeStyle = "#009933"; // color of grid lines
+  context.strokeStyle = "rgba(0, 0, 0, 0.2)"; // color of grid lines
   context.beginPath();
   // print Parameters on X axis, and grid lines on the graph
   context.fillText(xAxis[1], x, columnSize - margin);
+  context.fillStyle = "black";
   for (i = 1; i <= sections; i++) {
     var x = i * xScale;
-    if (i % 40 === 0) {
-      // console.log(xAxis[i].date.month)
-      context.fillText(xAxis[i], x, columnSize - margin);
+    if (i % 60 === 0) {
+      var now = new Date().toJSON().slice(0,10);
+      // console.log(now);
+      const date=new Date(xAxis[i])
+      // console.log(date);
+      months = new Array('January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December')
+      let m = months[date.getMonth()]
+      context.fillText(m, x-15, columnSize-10);
       context.moveTo(x, columnSize);
       context.lineTo(x, canvas.height - margin);
+      // context.fillStyle= 'grey';
     }
   }
   // print row header and draw horizontal grid lines
   var count = 0;
   // console.log(Val_max);
+  // for (scale = 0; scale >= 100000; scale = scale + 30) {
   for (scale = Val_max; scale >= Val_min; scale = scale - stepSize) {
     var y = 60 + yScale * count * stepSize;
-    if (count % 400 === 0 || count === Math.floor(Val_max / stepSize)) {
-      context.fillText(scale, margin, y + margin);
+    if (count % 500 === 0 || count === Math.floor(Val_max / stepSize)) {
+      console.log(100*Math.round(scale/100))
+      context.fillText(2500*Math.floor(scale/2500), margin+15, y + margin);
+      context.fillStyle = "black";
       context.moveTo(rowSize, y);
       context.lineTo(canvas.width, y);
     }
@@ -74,14 +78,14 @@ function plotData(dataSet) {
 
 const btn = document.querySelector(".fetch");
 const output = document.querySelector(".data");
-// const tbody = document.querySelector("tbody");
+const tbody = document.querySelector("tbody");
 let yAxis = [];
 let xAxis = [];
 
 function populateTable(d, i) {
   const tRow = document.createElement("tr");
-  if (i < 10) {
-    tRow.style.display = "block";
+  if (i < 14) {
+    tRow.style.display = "table-row";
   } else {
     tRow.style.display = "none";
     // tRow.classList.add("noDisp");
@@ -98,7 +102,7 @@ function showAll() {
   const rows = document.querySelectorAll("tr");
   rows.forEach((h) => {
     // console.log(h);
-    h.style.display='block';
+    h.style.display='table-row';
     // h.classList.add("disp");
   });
   var showAllBtn = document.querySelector("button");
